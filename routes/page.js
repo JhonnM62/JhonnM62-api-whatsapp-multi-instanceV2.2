de32426a75db20c80b50d6c35e0c7724c1ebd4a0
JhonnM62 <jonnymejia62@gmail.com>
2023-11-06 13:25:54 -0500

    api wpp two

--- routes/page.js
+++ routes/page.js
@@ -66,4 +66,34 @@ router.post("/upload", upload.single("file"), async (req, res) => {
     }
 });
 
+router.post("/upload2", upload.single("file"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res
+                .status(400)
+                .json({ error: "No se ha proporcionado ningún archivo" });
+        }
+
+        const imagePath = "./public/uploads/" + req.file.filename;
+
+        // Verifica si el archivo subido es un archivo de audio
+        if (req.file.mimetype.startsWith("audio/")) {
+            // Aquí deberías implementar la función convertAudio(url) que convierte el archivo de audio
+            const convertedAudioPath = await convertAudio(imagePath);
+            // Elimina el archivo original después de la conversión
+            await fs.unlink(imagePath);
+            const rutaCorregida = "./" + convertedAudioPath.replace(/\\/g, "/");
+            // Pasa la ruta del archivo de audio convertido a la vista
+            res.render("index", { imagePath: rutaCorregida });
+        } else {
+            res.render("index", { imagePath }); // Pasa imagePath a la vista
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({
+            error: "Error al cargar o convertir el archivo",
+        });
+    }
+});
+
 export default router;
