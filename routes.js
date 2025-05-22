858cae455db85a07c1d4dea1c738e4dd0be0bf03
JhonnM62 <jonnymejia62@gmail.com>
2025-05-22 14:36:36 -0500

    modificaciones en whatsapp.js, dependecias y nuevos archivos

--- routes.js
+++ routes.js
@@ -1,23 +1,23 @@
-import { Router } from 'express'
-import sessionsRoute from './routes/sessionsRoute.js'
-import chatsRoute from './routes/chatsRoute.js'
-import groupsRoute from './routes/groupsRoute.js'
-import miscRoute from './routes/miscRoute.js'
-import response from './response.js'
-import authenticationValidator from './middlewares/authenticationValidator.js'
+import { Router } from "express";
+import sessionsRoute from "./routes/sessionsRoute.js";
+import chatsRoute from "./routes/chatsRoute.js";
+import groupsRoute from "./routes/groupsRoute.js";
+import miscRoute from "./routes/miscRoute.js";
+import response from "./response.js";
+import page from "./routes/pageRoute.js";
+import { verifyToken } from "./login/src/middlewares/authJwt.js";
 
-const router = Router()
+const router = Router();
 
-// Use auth middleware for all routes
-router.use(authenticationValidator)
+router.use("/sessions", verifyToken, sessionsRoute);
+router.use("/chats", verifyToken, chatsRoute);
+router.use("/groups", verifyToken, groupsRoute);
+router.use("/misc", verifyToken, miscRoute);
 
-router.use('/sessions', sessionsRoute)
-router.use('/chats', chatsRoute)
-router.use('/groups', groupsRoute)
-router.use('/misc', miscRoute)
+router.use("/", page);
 
-router.all('*', (req, res) => {
-    response(res, 404, false, 'The requested url cannot be found.')
-})
+router.all("*", (req, res) => {
+    response(res, 404, false, "The requested url cannot be found.");
+});
 
-export default router
+export default router;
